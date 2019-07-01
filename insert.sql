db16c0c3ba72927448da95a7b8059e4c7b2c0d2d
Jay Patel <jay26597@gmail.com>
2019-07-01 13:02:29 -0400

    modify schema.sql for existing tables and create inserts for insert.sql

--- insert.sql
+++ insert.sql
@@ -1,1 +1,223 @@
 -- sample data (must have at least 3 stores, 20 customers, 50 purchases)
+
+-- STORES
+INSERT INTO boutique_coffee.Store (Store_ID, Name, Address, Store_Type, GPS_Long, GPS_Lat)
+VALUES ();
+
+INSERT INTO boutique_coffee.Store (Store_ID, Name, Address, Store_Type, GPS_Long, GPS_Lat)
+VALUES ();
+
+INSERT INTO boutique_coffee.Store (Store_ID, Name, Address, Store_Type, GPS_Long, GPS_Lat)
+VALUES ();
+
+-- CUSTOMERS
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
+VALUES ();
+
+-- PURCHASES
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
+
+INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
+VALUES ();
--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
 -- set up schema and tables for project
-DROP TABLE boutique_coffee.BuyCoffee;
-DROP TABLE boutique_coffee.PromoteFor;
-DROP TABLE boutique_coffee.HasPromotion;
-DROP TABLE boutique_coffee.OfferCoffee;
-DROP TABLE boutique_coffee.Purchase;
-DROP TABLE boutique_coffee.Customer;
-DROP TABLE boutique_coffee.MemberLevel;
-DROP TABLE boutique_coffee.Promotion;
-DROP TABLE boutique_coffee.Coffee;
-DROP TABLE boutique_coffee.Store;
+DROP TABLE IF EXISTS boutique_coffee.BuyCoffee CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.PromoteFor CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.HasPromotion CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.OfferCoffee CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.Purchase CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.Customer CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.MemberLevel CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.Promotion CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.Coffee CASCADE;
+DROP TABLE IF EXISTS boutique_coffee.Store CASCADE;
 
-DROP SCHEMA boutique_coffee;
+DROP SCHEMA IF EXISTS boutique_coffee;
 CREATE SCHEMA boutique_coffee;
 
 CREATE TABLE boutique_coffee.Store

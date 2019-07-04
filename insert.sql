0f998b01b8163d6bde35421c8fa698b7bf775d09
Jay Patel <jay26597@gmail.com>
2019-07-04 18:37:16 -0400

    compleated purchase inserts.

--- insert.sql
+++ insert.sql
@@ -2,26 +2,26 @@
 
 -- STORES INSERTS
 INSERT INTO boutique_coffee.Store (Store_ID, Name, Address, Store_Type, GPS_Long, GPS_Lat)
-VALUES (DEFAULT,'Starbucks','4022 Fifth Ave','Coffee', '37.3','20.8');
+VALUES (DEFAULT, 'Starbucks', '4022 Fifth Ave', 'Coffee', '37.3', '20.8');
 
 INSERT INTO boutique_coffee.Store (Store_ID, Name, Address, Store_Type, GPS_Long, GPS_Lat)
-VALUES (DEFAULT,'Crazy Mohca', '207 Oakland Ave', 'Coffee', '22.20', '45.01');
+VALUES (DEFAULT, 'Crazy Mohca', '207 Oakland Ave', 'Coffee', '22.20', '45.01');
 
 INSERT INTO boutique_coffee.Store (Store_ID, Name, Address, Store_Type, GPS_Long, GPS_Lat)
-VALUES (DEFAULT,'Arriviste Coffee Bar', '5730 Ellsworth Ave', 'Coffee', '80.23', '85.85');
+VALUES (DEFAULT, 'Arriviste Coffee Bar', '5730 Ellsworth Ave', 'Coffee', '80.23', '85.85');
 
 -- MEMBER LEVEL INSERTS
 INSERT INTO boutique_coffee.memberlevel(memberlevel_id, name, booster_factor)
-VALUES (DEFAULT, 'Regular', 5);
+VALUES (DEFAULT, 'Regular', .35);
 
 INSERT INTO boutique_coffee.memberlevel(memberlevel_id, name, booster_factor)
-VALUES (DEFAULT, 'Gold', 10);
+VALUES (DEFAULT, 'Gold', .70);
 
 INSERT INTO boutique_coffee.memberlevel(memberlevel_id, name, booster_factor)
-VALUES (DEFAULT, 'Platinum', 15);
+VALUES (DEFAULT, 'Platinum', 1.10);
 
 INSERT INTO boutique_coffee.memberlevel(memberlevel_id, name, booster_factor)
-VALUES (DEFAULT, 'Diamond', 20);
+VALUES (DEFAULT, 'Diamond', 1.89);
 
 
 -- CUSTOMERS INSERTS
@@ -62,7 +62,7 @@ INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, 
 VALUES (DEFAULT, 'Urijah', 'Ochoa', 'OU901@coffee.com', 1, 31);
 
 INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
-VALUES (DEFAULT,'Emely', 'Carney', 'CE258@coffee.com', 2, 29);
+VALUES (DEFAULT, 'Emely', 'Carney', 'CE258@coffee.com', 2, 29);
 
 INSERT INTO boutique_coffee.customer(customer_id, first_name, last_name, email, memberlevel_id, total_points)
 VALUES (DEFAULT, 'Rene', 'Fletcher', 'FR369@coffee.com', 3, 89);
@@ -87,151 +87,151 @@ VALUES (DEFAULT, 'Kyler', 'Gilbert', 'GK161@coffee.com', 3, 21);
 
 -- PURCHASES
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 1, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 2, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 3, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 4, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 5, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 6, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 7, 2, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 8, 2, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 9, 2, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 10, 2, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 11, 2, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 12, 2, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 13, 3, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 14, 3, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 15, 3, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 16, 3, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 17, 3, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 18, 3, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 19, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 20, 1, '06/30/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 1, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 2, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 3, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 4, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 5, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 6, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 7, 3, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 8, 3, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 9, 3, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 10, 3, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 11, 3, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 12, 3, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 13, 1, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 14, 1, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 15, 1, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 16, 1, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 17, 1, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 18, 1, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 19, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 20, 2, '07/01/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 1, 3, '07/02/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 2, 3, '07/02/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 3, 3, '07/02/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 8, 1, '07/02/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 9, 1, '07/02/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 10, 1, '07/02/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 13, 2, '07/03/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 14, 2, '07/03/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 19, 3, '07/03/2019');
 
 INSERT INTO boutique_coffee.purchase(purchase_id, customer_id, store_id, purchase_time)
-VALUES (DEFAULT,);
+VALUES (DEFAULT, 20, 3, '07/03/2019');

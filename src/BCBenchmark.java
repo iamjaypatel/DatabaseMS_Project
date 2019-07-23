59b8fb3c1cc618f46324488b429cd5592609f7c8
Jay Patel <jay26597@gmail.com>
2019-07-22 21:50:30 -0400

    5/14 Stresstests completed. Run on Empty DB

--- src/BCBenchmark.java
+++ src/BCBenchmark.java
@@ -1,3 +1,7 @@
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BCBenchmark {
     private static BoutiqueCoffee db;
 
@@ -14,6 +18,7 @@ public class BCBenchmark {
         db.setErrorLogger(s -> System.err.println(s));
 
         // Stress Test Methods and Time them.
+
         startTime = System.currentTimeMillis();
         stressTest_getCoffees();
         endTime = System.currentTimeMillis();
@@ -39,6 +44,30 @@ public class BCBenchmark {
         System.out.println("Execution time for addCoffee(): " + calcTime + " ms.");
 
         startTime = System.currentTimeMillis();
+        stressTest_addMemberLevel();
+        endTime = System.currentTimeMillis();
+        calcTime = endTime - startTime;
+        System.out.println("Execution time for addMemberLevel(): " + calcTime + " ms.");
+
+        startTime = System.currentTimeMillis();
+        stressTest_addStore();
+        endTime = System.currentTimeMillis();
+        calcTime = endTime - startTime;
+        System.out.println("Execution time for addStore(): " + calcTime + " ms.");
+
+        startTime = System.currentTimeMillis();
+        stressTest_offerCoffee();
+        endTime = System.currentTimeMillis();
+        calcTime = endTime - startTime;
+        System.out.println("Execution time for offerCoffee(): " + calcTime + " ms.");
+
+        startTime = System.currentTimeMillis();
+        stressTest_addCustomer();
+        endTime = System.currentTimeMillis();
+        calcTime = endTime - startTime;
+        System.out.println("Execution time for addCustomer(): " + calcTime + " ms.");
+
+        startTime = System.currentTimeMillis();
         stressTest_addPromotion();
         endTime = System.currentTimeMillis();
         calcTime = endTime - startTime;
@@ -49,24 +78,6 @@ public class BCBenchmark {
         endTime = System.currentTimeMillis();
         calcTime = endTime - startTime;
         System.out.println("Execution time for addPurchase(): " + calcTime + " ms.");
-
-        startTime = System.currentTimeMillis();
-        stressTest_addMemberLevel();
-        endTime = System.currentTimeMillis();
-        calcTime = endTime - startTime;
-        System.out.println("Execution time for addMemberLevel(): " + calcTime + " ms.");
-
-        startTime = System.currentTimeMillis();
-        stressTest_addCustomer();
-        endTime = System.currentTimeMillis();
-        calcTime = endTime - startTime;
-        System.out.println("Execution time for addCustomer(): " + calcTime + " ms.");
-
-        startTime = System.currentTimeMillis();
-        stressTest_addStore();
-        endTime = System.currentTimeMillis();
-        calcTime = endTime - startTime;
-        System.out.println("Execution time for addStore(): " + calcTime + " ms.");
 
         startTime = System.currentTimeMillis();
         stressTest_promoteFor();
@@ -80,11 +91,7 @@ public class BCBenchmark {
         calcTime = endTime - startTime;
         System.out.println("Execution time for hasPromotion(): " + calcTime + " ms.");
 
-        startTime = System.currentTimeMillis();
-        stressTest_offerCoffee();
-        endTime = System.currentTimeMillis();
-        calcTime = endTime - startTime;
-        System.out.println("Execution time for offerCoffee(): " + calcTime + " ms.");
+
 
         startTime = System.currentTimeMillis();
         stressTest_topStores();
@@ -98,61 +105,114 @@ public class BCBenchmark {
         calcTime = endTime - startTime;
         System.out.println("Execution time for topCustomers(): " + calcTime + " ms.");
 
+
     }
 
     private static void stressTest_getCoffees() {
+        System.out.println("\n--- GET COFFEES 1000 INSERTS ---");
 
     }
 
     private static void stressTest_getCoffeesByKeyword() {
+        System.out.println("\n--- GET COFFEES BY KEYWORD ---");
 
     }
 
     private static void stressTest_getPointsByCustomerId() {
+        System.out.println("\n--- GET POINTS BY CUSTOMER ID ---");
 
     }
 
     private static void stressTest_addCoffee() {
-
+        System.out.println("\n--- ADD COFFEE 1000 Inserts ---");
+        for (int i = 0; i < 1000; i++){
+            db.addCoffee("Coffee Name", "Description", 11, 7.79, 80, 100);
+        }
     }
 
     private static void stressTest_addCustomer() {
+        System.out.println("\n--- ADD CUSTOMER 1000 Inserts ---");
+        int [] ml = new int[1001];
+        double [] pts = new double[1001];
+        for (int i = 1; i < 1001; i++){
+            ml[i] = i;
+            pts[i] = i;
+        }
+        for(int k = 1; k < 1001; k++){
+            db.addCustomer("FN " + k, "LN " + k, "FN.LN" + k + "@gmail.com", ml[k], pts[k]);
+        }
 
     }
 
     private static void stressTest_addMemberLevel() {
-
+        System.out.println("\n--- ADD MEMBER LEVEL 1000 Inserts ---");
+        for (int i = 1; i < 1001; i++){
+            db.addMemberLevel("MemberLevel "+ i, i * 2);
+        }
     }
 
     private static void stressTest_addStore() {
-
+        System.out.println("\n--- ADD STORE 1000 Inserts ---");
+        for (int i = 0; i < 1000; i++){
+            db.addStore("Store " + i, "Address Store " + i, "Store Type", i * 4.12, i * 2.25);
+        }
     }
 
     private static void stressTest_hasPromotion() {
+        System.out.println("\n--- HAS PROMOTION 1000 INSERTS ---");
 
     }
 
     private static void stressTest_promoteFor() {
+        System.out.println("\n--- PROMOTE FOR 1000 INSERTS ---");
 
     }
 
     private static void stressTest_addPromotion() {
+        System.out.println("\n--- ADD PROMOTION 1000 INSERTS ---");
 
     }
 
     private static void stressTest_offerCoffee() {
+        System.out.println("\n--- OFFER COFFEE 1000 INSERTS ---");
+        for (int i = 1; i < 1001; i++){
+            db.offerCoffee(i, i);
+        }
 
     }
 
     private static void stressTest_addPurchase() {
+        System.out.println("\n--- ADD PURCHASE 1000 INSERTS ---");
+        /*
+        Date d = Date.valueOf("2019-07-20");
+        List<Integer> coffees = new ArrayList<Integer>(1000);
+        List<Integer> purchased = new ArrayList<Integer>(1000);
+        List<Integer> redeemed = new ArrayList<Integer>(1000);
 
+        for (int i = 1; i < 1000; i++){
+            for(int c = 1; c < 7; c++){
+                coffees.add(c);
+            }
+        }
+        for (int j = 1; j < 1000; j++){
+            purchased.add(j);
+        }
+        for(int k = 1; k < 2; k++){
+            redeemed.add(k);
+        }
+        for(int n = 1; n < 1000; n++){
+            db.addPurchase(n, n, d, coffees, purchased, redeemed);
+        }
+        */
     }
 
     private static void stressTest_topStores() {
+        System.out.println("\n--- TOP SCORES ---");
 
     }
 
     private static void stressTest_topCustomers() {
+        System.out.println("\n--- TOP CUSTOMERS ---");
 
     }
 }

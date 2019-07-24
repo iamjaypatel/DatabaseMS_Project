243236cb729e51d54b0a557ba6407f6ce704aeab
Jay Patel <jay26597@gmail.com>
2019-07-23 23:32:10 -0400

    First Panel GUI completed and created second panel

--- src/BCApp.java
+++ src/BCApp.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class BCApp {
     private JPanel mainPanel; // Member ID Panel
@@ -11,11 +13,26 @@ public class BCApp {
     private JLabel lblFirstName;
     private JLabel lblLastName;
     private JLabel lblEmail;
+    private JTextField txtMemberID;
+    private JButton btnFindMemID;
+    private JLabel lblMemberID;
+    private JLabel lblRetMemID;
+    private JLabel lblretValMemID;
+    private JRadioButton radExisting;
+    private JRadioButton radNewMember;
 
     private static BoutiqueCoffee boutiqueCoffee;
 
     private JPanel secondPanel; // goes here after finding member
     public BCApp() {
+        txtEmail.setEditable(false);
+        txtLastName.setEditable(false);
+        txtFirstName.setEditable(false);
+        btnAddMember.setEnabled(false);
+        txtMemberID.setEditable(false);
+        btnFindMemID.setEnabled(false);
+        lblRetMemID.setVisible(false);
+
         boutiqueCoffee.addMemberLevel("Regular", 0.35);
         boutiqueCoffee.addMemberLevel("Gold", 0.7);
         boutiqueCoffee.addMemberLevel("Platinum", 1.1);
@@ -31,7 +48,81 @@ public class BCApp {
             public void actionPerformed(ActionEvent e) {
                 txtFirstName.getAccessibleContext();
                 boutiqueCoffee.addCustomer(txtFirstName.getText(), txtLastName.getText(), txtEmail.getText(),1,0);
-                JOptionPane.showMessageDialog(null, "Customer Added");
+            }
+        });
+        btnFindMemID.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double ret;
+                txtMemberID.getAccessibleContext();
+                ret = boutiqueCoffee.getMemberID(Integer.parseInt(txtMemberID.getText()));
+                lblretValMemID.setText(Double.toString(ret));
+
+                if (ret != -1.0){
+                    BCAppDialog dialog = new BCAppDialog();
+                    dialog.pack();
+                    dialog.setVisible(true);
+                } else {
+                    lblretValMemID.setText("Member NOT FOUND");
+                }
+            }
+        });
+        txtMemberID.addKeyListener(new KeyAdapter() {
+            /**
+             * Invoked when a key has been pressed.
+             *
+             * @param e
+             */
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                try {
+                    int i = Integer.parseInt(txtMemberID.getText());
+                } catch (NumberFormatException e1) {
+                }
+            }
+        });
+        radExisting.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtEmail.setEditable(false);
+                txtLastName.setEditable(false);
+                txtFirstName.setEditable(false);
+                btnAddMember.setEnabled(false);
+
+                txtMemberID.setEditable(true);
+                btnFindMemID.setEnabled(true);
+                lblRetMemID.setVisible(true);
+
+            }
+        });
+        radNewMember.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtEmail.setEditable(true);
+                txtLastName.setEditable(true);
+                txtFirstName.setEditable(true);
+                btnAddMember.setEnabled(true);
+                lblRetMemID.setVisible(true);
+
+
+                txtMemberID.setEditable(false);
+                btnFindMemID.setEnabled(false);
             }
         });
     }
@@ -40,9 +131,11 @@ public class BCApp {
         // TODO: place custom component creation code here
         mainPanel = new JPanel();
         btnAddMember = new JButton();
-        txtFirstName = new JTextField();
-        txtLastName = new JTextField();
-        txtEmail = new JTextField();
+        txtFirstName = new JTextField(20);
+        txtLastName = new JTextField(20);
+        txtEmail = new JTextField(20);
+        txtMemberID = new JTextField();
+
     }
 
     public static void main(String[] args) {
@@ -54,12 +147,11 @@ public class BCApp {
             System.exit(-1);
         }
 
-        JFrame frame = new JFrame("Boutique Coffee: Add Customer");
+        JFrame frame = new JFrame("Boutique Coffee");
         frame.setContentPane(new BCApp().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
-        JFrame frame1 = new JFrame();
     }
 }
